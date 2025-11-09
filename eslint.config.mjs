2843940109b05ebc2b06c2f4c2aed65346c82aca
Krzysztof Bzowski <kbzowski@agh.edu.pl>
2025-11-09 13:15:22 +0100

    Dodanie reguły formatowania wcięć do ESLint
    - Dodano regułę 'indent': 'warn' dla automatycznego sprawdzania wcięć
    - Przeformatowano plik zgodnie z nowymi regułami

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,25 +3,26 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    rules: {
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'sort-imports': ['error', {
-        'ignoreCase': false,
-        'ignoreDeclarationSort': true,
-        'ignoreMemberSort': false,
-      }],
+    ...nextVitals,
+    ...nextTs,
+    {
+        rules: {
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
+            'sort-imports': ['error', {
+                'ignoreCase': false,
+                'ignoreDeclarationSort': true,
+                'ignoreMemberSort': false,
+            }],
+            'indent': 'warn'
+        },
     },
-  },
-  globalIgnores([
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+    globalIgnores([
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+    ]),
 ]);
 
 export default eslintConfig;
